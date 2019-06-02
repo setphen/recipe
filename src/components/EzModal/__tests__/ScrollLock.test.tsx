87eb460bd344fb8731218610df10e24f98bbf221
Craig Cavalier <craigcav@gmail.com>
2019-06-02 12:14:26 -0400

    switch to Global component for global styles

--- src/components/EzModal/__tests__/ScrollLock.test.tsx
+++ src/components/EzModal/__tests__/ScrollLock.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {cleanup} from 'react-testing-library';
 import 'jest-dom/extend-expect';
-import ScrollLock, {preventScroll} from '../ScrollLock';
+import ScrollLock from '../ScrollLock';
 
 import {fullRender} from '../../../jest-globals';
 
@@ -9,15 +9,19 @@ afterEach(cleanup);
 
 describe('EzModal', () => {
   it('sets the html to overflow hidden when open', () => {
-    const {baseElement} = fullRender(<ScrollLock />);
+    fullRender(<ScrollLock />);
 
-    expect(baseElement.parentNode.classList).toContain(preventScroll);
+    expect(
+      window.getComputedStyle(document.documentElement).getPropertyValue('overflow')
+    ).toContain('hidden');
   });
 
   it('does not set overflow hidden on the html when it is not open', () => {
-    const {baseElement, unmount} = fullRender(<ScrollLock />);
+    const {unmount} = fullRender(<ScrollLock />);
     unmount();
 
-    expect(baseElement.parentNode.classList).not.toContain(preventScroll);
+    expect(
+      window.getComputedStyle(document.documentElement).getPropertyValue('overflow')
+    ).not.toContain('hidden');
   });
 });
