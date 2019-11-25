2a87c21c015754c4b7f5bd8643a37173fbc63743
Craig Cavalier <craigcav@gmail.com>
2019-11-25 15:14:30 -0500

    create babel-plugin for linaria
    this plugin runs linaria and writes a .css file per module. Downstream apps/consumers can then use css-module plugins for their relevant build environment (webpack/parcel/rollup etc) to code-split/bundle.

---
+++ linaria.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+const {basename, relative, dirname} = require('path');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const extract = require('linaria/lib/babel/extract');
+const {addSideEffect} = require('@babel/helper-module-imports');
+
+module.exports = function linaria(context, options) {
+  const {visitor} = extract(context, {
+    displayName: false,
+    evaluate: true,
+    ignore: /node_modules/,
+    ...options,
+  });
+
+  return {
+    name: 'linaria-extract',
+    visitor: {
+      Program: {
+        enter(path, state) {
+          visitor.Program.enter(path, state);
+          //
+        },
+        exit(path, state) {
+          // if no styles were create for this file, no more work to do
+          if (!Object.keys(state.rules).length) return;
+
+          const {filename} = state.file.opts;
+
+          const outputFilename = relative(
+            process.cwd(),
+            filename.replace(/\.[^.]+$/, '.linaria.css')
+          );
+
+          let cssText = '';
+
+          Object.keys(state.rules).forEach(selector => {
+            cssText += `${selector} {${state.rules[selector].cssText}}\n`;
+          });
+
+          if (!cssText) return;
+
+          mkdirp.sync(dirname(outputFilename));
+
+          fs.writeFileSync(outputFilename, cssText);
+
+          addSideEffect(path, './' + basename(outputFilename));
+        },
+      },
+    },
+  };
+};
+/* eslint-enable */
