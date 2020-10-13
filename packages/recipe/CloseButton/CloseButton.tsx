0401401f8cd54005a4009915a1cc436e4efefca7
Craig Cavalier <craigcav@gmail.com>
2020-10-13 16:45:15 -0400

    make /CloseButton stand-alone package

--- packages/recipe/CloseButton/CloseButton.tsx
+++ packages/recipe/CloseButton/CloseButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import styled from '@recipe/styled';
 import {base} from './CloseButton.styles';
-import styled from '../../themes/styled';
 
 const CloseButton = styled.button<any>(base);
 
