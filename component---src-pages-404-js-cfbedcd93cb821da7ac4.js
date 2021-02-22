b3cb9d4a99a3ea4cbd104cb47f051cbecb28a9c5
Craig Cavalier <craigcav@gmail.com>
2021-02-22 12:46:05 -0500

    Updates

---
+++ component---src-pages-404-js-cfbedcd93cb821da7ac4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(t,n,s){"use strict";s.r(n);s("q1tI");var e=s("qKvR");n.default=function(){return Object(e.jsx)("div",null,Object(e.jsx)("h1",null,"NOT FOUND"),Object(e.jsx)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-cfbedcd93cb821da7ac4.js.map
