45b499c52d9576fbfbfc00d7d2b868dc0d0beca0
Craig Cavalier <craigcav@gmail.com>
2021-06-22 15:51:38 -0400

    disable ssr

--- src/snitches/index.tsx
+++ src/snitches/index.tsx
@@ -1,27 +1,7 @@
 /* eslint-disable filenames/match-exported */
-import React, {createContext, useContext} from 'react';
-// import ReactDOM from 'react-dom';
 
-const Context = createContext(null);
-
-const isNodeEnvironment = (): boolean => {
-  // https://nodejs.org/api/process.html#process_process_release
-  return typeof process !== 'undefined' && process?.release?.name === 'node';
-};
-
-const Tag = ({ruleset}) => <style dangerouslySetInnerHTML={{__html: ruleset?.toString() || ''}} />;
-
-const Style = ({children = null, ruleset}: any) => {
-  const context = useContext(Context);
-
-  if (context || !isNodeEnvironment()) return children || null;
-
-  return (
-    <Context.Provider value>
-      {children}
-      <Tag ruleset={ruleset} />
-    </Context.Provider>
-  );
+const Style = ({children = null}: any) => {
+  return children || null;
 };
 
 export default Style;
