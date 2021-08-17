67ae549088f953dffcb2b781b3be17c091d28796
danidewitt <danidewitt@gmail.com>
2021-08-17 10:54:07 -0400

    wip

--- doc-site/src/components/Playground/Preview.js
+++ doc-site/src/components/Playground/Preview.js
@@ -6,68 +6,20 @@ import {createUrl} from 'playroom/utils';
 import Code from '../Code';
 
 const codeIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="16"
-    width="16"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-    />
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <use xlinkHref="#code" />
   </svg>
 );
 
 const previewIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="16"
-    width="16"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-    />
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-    />
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <use xlinkHref="#preview" />
   </svg>
 );
 
 const playIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="16"
-    width="16"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-    />
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <use xlinkHref="#play" />
   </svg>
 );
 
