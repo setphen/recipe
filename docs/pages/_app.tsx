a7d04131f452ee7ed5b30bbee0d11d52249176bd
Craig Cavalier <craigcav@gmail.com>
2021-09-08 11:52:14 -0400

    add google analytics

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,14 @@
 import {AppProps} from 'next/app';
+import {useEffect} from 'react';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {gtmId: 'GTM-TJRFGNR'};
 
 function MyApp({Component, pageProps}: AppProps) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
