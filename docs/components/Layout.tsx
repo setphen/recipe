7b74e33691f8d7878548a58381fdcab139f7e787
Craig Cavalier <craigcav@gmail.com>
2021-09-09 10:58:55 -0400

    add site-wide theme toggle

--- docs/components/Layout.tsx
+++ docs/components/Layout.tsx
@@ -1,13 +1,18 @@
 import Head from 'next/head';
-import {EzAppLayout, EzNavigation} from '@ezcater/recipe';
+import {EzAppLayout, EzNavigation, EzProvider} from '@ezcater/recipe';
+import {theme as marketplaceTheme} from '@recipe-ui/theme-marketplace';
 import {docsRoutes} from '../lib/docsRoutes';
 import recipeLogo from '../public/images/recipe-logo.svg';
 import NextLink from './NextLink';
 import Sprites from './Sprites';
+import {useMarketplaceTheme, useToggleMarketplaceTheme} from '../pages/_app';
 
 export default function Layout({children}: any) {
+  const isMarketplaceThemeSelected = useMarketplaceTheme();
+  const toggleMarketplaceTheme = useToggleMarketplaceTheme();
+
   return (
-    <>
+    <EzProvider theme={isMarketplaceThemeSelected ? marketplaceTheme : undefined}>
       <Head>
         <link
           href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i"
@@ -28,10 +33,16 @@ export default function Layout({children}: any) {
             links: route.pages?.map(({title, path}: any) => ({label: title, href: path})),
             ...(route.external ? {href: route.path} : {as: NextLink, to: route.path}),
           }))}
+          utilityLinks={[
+            {
+              label: `Use ${isMarketplaceThemeSelected ? 'fulfillment' : 'marketplace'} theme`,
+              onClick: toggleMarketplaceTheme,
+            },
+          ]}
         >
           {children}
         </EzNavigation>
       </EzAppLayout>
-    </>
+    </EzProvider>
   );
 }
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,14 +1,48 @@
 import {AppProps} from 'next/app';
-import {useEffect} from 'react';
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 import TagManager from 'react-gtm-module';
 
 const tagManagerArgs = {gtmId: 'GTM-TJRFGNR'};
+
+const ThemeState = createContext(false);
+const ThemeToggle = createContext<Dispatch<SetStateAction<boolean>>>(() => {});
+
+function ThemeProvider(props: any) {
+  const [isMarketplaceThemeSelected, showMarketplaceTheme] = useState(false);
+
+  return (
+    <ThemeState.Provider value={isMarketplaceThemeSelected}>
+      <ThemeToggle.Provider value={showMarketplaceTheme}>{props.children}</ThemeToggle.Provider>
+    </ThemeState.Provider>
+  );
+}
+
+export function useMarketplaceTheme(): boolean {
+  return useContext(ThemeState);
+}
+
+export function useToggleMarketplaceTheme(): () => void {
+  const showMarketplaceTheme = useContext(ThemeToggle);
+  return useCallback(() => showMarketplaceTheme(selected => !selected), [showMarketplaceTheme]);
+}
 
 function MyApp({Component, pageProps}: AppProps) {
   useEffect(() => {
     TagManager.initialize(tagManagerArgs);
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 export default MyApp;
