a6233187fa061d51ad1f8706f5343633c0050938
Craig Cavalier <craigcav@gmail.com>
2021-09-20 09:44:07 -0400

    delete old doc-site

--- doc-site/src/components/Playground/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Preview from './Preview';
-
-const Playground = ({code, scope, language}) => (
-  <Preview code={code} scope={scope} language={language} />
-);
-
-export default Playground;
