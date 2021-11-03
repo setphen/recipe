b1f7f9cf5befa706a4f2ce829821da26252f0950
Peter Grippi <peter.grippi@ezcater.com>
2021-11-03 14:05:59 -0400

    Next.js debugging
    * Upgrade @ezcater/snitches to fix an issue with SVG style hydration
    * Remove the custom Symbol polyfill in favor of relying on the consuming application to supply it

--- packages/@stitches/core/dist/index.js
+++ packages/@stitches/core/dist/index.js
@@ -1,14 +1,6 @@
-if (typeof globalThis === 'undefined') window.globalThis = window;
-
-const isIE11 = typeof window !== `undefined` && !!window.MSInputMethodContext;
-const Symbol = isIE11
-  ? {
-      for(x) {
-        return x;
-      },
-      iterator: '__symbol:iterator0.97589819915729773',
-    }
-  : globalThis.Symbol;
+if (typeof globalThis === 'undefined' && typeof window !== 'undefined') {
+  window.globalThis = window;
+}
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
